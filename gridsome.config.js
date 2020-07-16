b948b65167b73d733a5c2f749c4818990a9e4683
sharvilak <sharvilak@e9ine.com>
2020-07-16 20:13:55 +0530

    feat: initial setup with gridsome starter

---
+++ gridsome.config.js
@@ -0,0 +1,32 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'e9 Blog',
+  siteDescription: 'We\'re the technology team at e9, through the medium of the blog, we keep our customers and surrounding audience up to date with how we build, scale, and operate our platform and teams.',
+  templates: {
+    Post: '/blog/:title',
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/posts/**/*.md',
+        typeName: 'Post',
+      }
+    }
+  ],
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      plugins: [
+        '@gridsome/remark-prismjs'
+      ]
+    }
+  },
+}
